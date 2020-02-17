4a6c3a6825154239ffdb789dbeba80d7b416d9cb
Steven Hofheins <stevenisinsane@gmail.com>
2020-02-16 20:03:02 -0700

    fixing

--- src/pages/StatsDashboard.js
+++ src/pages/StatsDashboard.js
@@ -1,40 +1,36 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
 function StatsDashboard() {
-    const GitHubAPI = ' 9de8795f8f46d7aac72c1699ab021c8ca4df4568'
+  // const trythis = () => {
+  //     fetch('  https://api.github.com/', {
+  //         method: 'GET',
+  //         headers: {
+  //             access_token: 'token ,
+  //             "Content-Type": "application/json",
+  //             "Access-Control-Allow-Origin": "*",
+  //             "Access-Control-Allow-Headers": "Content-type"
+  //         }
+  //     })
+  //         .then(response => response.json())
+  //         .then(data => data.map())
+  // }
 
-    // const trythis = () => {
-    //     fetch('  https://api.github.com/', {
-    //         method: 'GET',
-    //         headers: {
-    //             access_token: 'token 9de8795f8f46d7aac72c1699ab021c8ca4df4568',
-    //             "Content-Type": "application/json",
-    //             "Access-Control-Allow-Origin": "*",
-    //             "Access-Control-Allow-Headers": "Content-type"
-    //         }
-    //     })
-    //         .then(response => response.json())
-    //         .then(data => data.map())
-    // }
+  // useEffect(() => {
+  //     fuckthiss()
+  // }, [])
+  async function fuckthiss() {
+    const url = "https://api.github.com/users/tecdef";
+    const response = await fetch(url);
+    const result = await response.json();
+    console.log(result);
+  }
 
-    // useEffect(() => {
-    //     fuckthiss()
-    // }, [])
-    async function fuckthiss() {
-        const url = 'https://api.github.com/users/tecdef'
-        const response = await fetch(url)
-        const result = await response.json()
-        console.log(result)
-    }
-
-
-
-    return (
-        <div>
-            <button onClick={ fuckthiss }>click me</button>
-            {/* <a href="https://wakatime.com"><img src="https://wakatime.com/share/@tecdef/49aeae99-cd56-4ccf-bed5-46ebaf003eb0.png" /></a> */ }
-        </div>
-    )
+  return (
+    <div>
+      <button onClick={fuckthiss}>click me</button>
+      {/* <a href="https://wakatime.com"><img src="https://wakatime.com/share/@tecdef/49aeae99-cd56-4ccf-bed5-46ebaf003eb0.png" /></a> */}
+    </div>
+  );
 }
 
-export default StatsDashboard
+export default StatsDashboard;

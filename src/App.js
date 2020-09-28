bf418170ef90759bf0b0d3ea31d17df060b03d8a
Steven Hofheins <stevenisinsane@gmail.com>
2020-09-28 15:37:45 -0600

    moved medium feed to its own component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
 import React from "react";
-const Parser = require("rss-parser");
-const parser = new Parser();
+import { MediumFeed } from "./components/MediumFeed";
 
 function App() {
-  const [data, setData] = React.useState();
-  const [error, setError] = React.useState(false);
-  const [loading, setLoading] = React.useState(true);
-  const feed_url = `https://medium.com/feed/@steven_creates`;
-  const CORS_PROXY = `https://cors-anywhere.herokuapp.com/`;
-
-  React.useEffect(() => {
-    (async () => {
-      try {
-        let feed = await parser.parseURL(CORS_PROXY + feed_url);
-        setData(feed);
-        setLoading(false);
-      } catch (err) {
-        setError(err);
-      }
-    })();
-  }, []);
-
   return (
     <div className='App'>
-      {data && (
-        <div>
-          {data.items.map((medium) => (
-            <div>{medium.title}</div>
-          ))}
-        </div>
-      )}
+      <MediumFeed />
     </div>
   );
 }
---
+++ src/components/MediumFeed.js
@@ -0,0 +1,35 @@
+import React from "react";
+const Parser = require("rss-parser");
+const parser = new Parser();
+
+export const MediumFeed = () => {
+  const [data, setData] = React.useState();
+  const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
+  const feed_url = `https://medium.com/feed/@steven_creates`;
+  const CORS_PROXY = `https://cors-anywhere.herokuapp.com/`;
+
+  React.useEffect(() => {
+    (async () => {
+      try {
+        let feed = await parser.parseURL(CORS_PROXY + feed_url);
+        setData(feed);
+        setLoading(false);
+      } catch (err) {
+        setError(err);
+      }
+    })();
+  }, []);
+
+  return (
+    <div>
+      {data && (
+        <div>
+          {data.items.map((medium) => (
+            <div>{medium.title}</div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};

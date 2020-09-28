0f4398285d6792b604c64414d88e9175decc0928
Steven Hofheins <stevenisinsane@gmail.com>
2020-09-28 15:59:41 -0600

    added container and grid

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import { MediumFeed } from "./components/MediumFeed";
 
 function App() {
   return (
-    <div className='App'>
-      <MediumFeed />
+    <div className='app-container'>
+      <div className='first-column'></div>
+      <div className='second-column'>
+        <MediumFeed />
+      </div>
+      <div className='third-column'>
+        <MediumFeed />
+      </div>
     </div>
   );
 }

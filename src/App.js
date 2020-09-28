25818107ea971429da4f785fb3bf5aecbd06d61e
Steven Hofheins <stevenisinsane@gmail.com>
2020-09-28 16:01:42 -0600

    added row to first column in grid

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import { MediumFeed } from "./components/MediumFeed";
 function App() {
   return (
     <div className='app-container'>
-      <div className='first-column'></div>
+      <div className='first-column'>
+        <div className='first-column-row'>1st Row</div>
+        <div className='first-column-second-row'>2nd Row</div>
+      </div>
       <div className='second-column'>
         <MediumFeed />
       </div>

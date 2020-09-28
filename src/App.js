2d35217e961f526aedf71ee4b73dd649e24f7767
Steven Hofheins <stevenisinsane@gmail.com>
2020-09-28 16:37:53 -0600

    added styling to current info section

--- src/App.js
+++ src/App.js
@@ -11,6 +11,16 @@ function App() {
         </div>
         <div className='first-column-second-row'>
           <div className='name-class'>Steven Hofheins</div>
+          <div className='about-container'>
+            <div className='about-item'>
+              <span className='item-title'>Location</span>
+              <span className='item-desc'>Utah</span>
+            </div>
+            <div className='about-item'>
+              <span className='item-title'>Company</span>
+              <span className='item-desc'>Adobe</span>
+            </div>
+          </div>
         </div>
       </div>
       <div className='second-column'>
